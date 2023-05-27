097ae9843f70e84003c8bc034e2399454ee7b61f
moviewebsit <134695564+moviewebsit@users.noreply.github.com>
2023-05-27 10:13:47 +0530

    Add files via upload

---
+++ click.js
@@ -0,0 +1,5 @@
+document.getElementById("clickButton").addEventListener("click", function() {
+  // Perform desired action here
+  // For example, you can navigate to another page:
+  window.location.href = "https://example.com";
+});
---
+++ search.js
@@ -0,0 +1,6 @@
+document.getElementById("searchButton").addEventListener("click", function(event) {
+  event.preventDefault(); // Prevent form submission
+  var searchQuery = document.getElementById("searchInput").value;
+  // Perform search logic using the searchQuery value
+  // Update the page with the search results
+});
